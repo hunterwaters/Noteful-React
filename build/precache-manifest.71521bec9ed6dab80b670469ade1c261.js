d2f37614badd6449bf6d5f4db4f9ce8f5fb48fdb
hunterwaters <55896961+hunterwaters@users.noreply.github.com>
2020-04-09 11:26:27 -0700

    Add files via upload

---
+++ build/precache-manifest.71521bec9ed6dab80b670469ade1c261.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c9055a1bf8412ffb7edf612689a4ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e96c07609fe0de386aa",
+    "url": "/static/css/main.aca7093e.chunk.css"
+  },
+  {
+    "revision": "a5108384b00fe3ba84e4",
+    "url": "/static/js/2.89887a86.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.89887a86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e96c07609fe0de386aa",
+    "url": "/static/js/main.548c5e7b.chunk.js"
+  },
+  {
+    "revision": "71877d0fd403952ec24f",
+    "url": "/static/js/runtime-main.4540848b.js"
+  }
+]);
---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+    //API_ENDPOINT: `https://shrouded-hamlet-39949.herokuapp.com/api/`
+    //API_ENDPOINT: `http://localhost:8000/api/`
+    //API_ENDPOINT: `https://immense-ocean-19669.herokuapp.com/`
+    API_ENDPOINT: `https://thawing-taiga-78223.herokuapp.com/api/`
+  }
